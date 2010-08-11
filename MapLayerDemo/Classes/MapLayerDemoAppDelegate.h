69c59e48fb080ab3d24e7f2f988e7bc31def9b56
Mike Tigas <mike@tig.as>
2010-08-10 23:58:35 -0700

    Initial commit, 'blank' app.

---
+++ MapLayerDemo/Classes/MapLayerDemoAppDelegate.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface MapLayerDemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
