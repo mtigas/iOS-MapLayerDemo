163b19a5e8bb70a23eb18fa97a550199c6345876
Mike Tigas <mike@tig.as>
2010-08-11 00:04:32 -0700

    An empty map view.

--- MapLayerDemo/Classes/MapLayerDemoAppDelegate.m
+++ MapLayerDemo/Classes/MapLayerDemoAppDelegate.m
@@ -1,4 +1,5 @@
 #import "MapLayerDemoAppDelegate.h"
+#import <MapKit/MapKit.h>
 
 @implementation MapLayerDemoAppDelegate
 
@@ -11,61 +12,25 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
-    // Override point for customization after application launch.
+
 	
+    MKMapView *mapView = [[MKMapView alloc] initWithFrame:window.bounds];
+    CLLocationCoordinate2D defaultPoint = CLLocationCoordinate2DMake(38.9517053, -92.3340724);
+    MKCoordinateSpan defaultSpan = MKCoordinateSpanMake(40.0f, 40.0f);
+    MKCoordinateRegion region = MKCoordinateRegionMake(defaultPoint, defaultSpan);
+    [mapView setRegion:region animated:FALSE];
+    [mapView regionThatFits:region];
+        
+    [window addSubview:mapView];
     [window makeKeyAndVisible];
-	
+    
+    [mapView release];
+    
 	return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    /*
-     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
-     */
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    /*
-     Called when the application is about to terminate.
-     See also applicationDidEnterBackground:.
-     */
-}
-
-
 #pragma mark -
 #pragma mark Memory management
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-    /*
-     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
-     */
-}
-
 
 - (void)dealloc {
     [window release];
